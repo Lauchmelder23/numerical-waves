a0a6527280603ca41e1945dc5f838ab556042411
Robert <robert.trololo@gmail.com>
2021-03-14 13:07:40 +0100

    Add project files.

---
+++ numerical-waves/numerical-waves.cpp
@@ -0,0 +1,12 @@
+﻿// numerical-waves.cpp : Defines the entry point for the application.
+//
+
+#include "numerical-waves.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
